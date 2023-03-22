1f0250ecfdd6279167389f9df0f9d7d1837761ee
Alfoncemokua <mokuaalfonce@gmail.com>
2023-03-22 04:10:34 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * 
+ * Description: 'computes the absolut value of an integer'
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
