e314aff39126fd706837de35bb072dc8fb089cc5
Alfoncemokua <mokuaalfonce@gmail.com>
2023-03-22 03:27:25 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,9 +4,8 @@
  *
  * @n: function parameter
  *
- * Return: 1, 0 and -1
+ * return: 1, 0 and -1
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -28,7 +27,6 @@ int print_sign(int n)
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
-		return (-1);
+		return (0);
 	}
 }
-
